10e7055b6cd789a8b8579567ab1d8e9f36f58f54
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2016-06-21 13:14:17 +0200

    Add test script for jobfile parsing.

---
+++ python/test_HRM.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+"""Simple test script for the HRM class.
+
+Run it from the directory that contains 'HRM.py' after setting your
+PYTHONPATH accordingly:
+
+export PYTHONPATH=$PYTHONPATH:./
+"""
+
+try:
+    import HRM
+except ImportError:
+    raise SystemExit(__doc__)
+
+# the reload statement is here so we can us the script in subsequent calls
+# during a single IPython session:
+reload(HRM)
+
+jobs_ok = [
+    '../jobfiles/sandbox/deconvolution_job.cfg'
+]
+
+print('Testing correct job description files:')
+for jobfile in jobs_ok:
+    job = HRM.JobDescription(jobfile, 'file')
+    print(" - Parsing worked without errors on '%s'." % jobfile)
+
+jobs_broken = [
+    '../jobfiles/testing/broken_sec_deconvolution.cfg',
+    '../jobfiles/testing/broken_sec_inputfiles.cfg',
+    '../jobfiles/testing/broken_sec_jobfile.cfg'
+]
+
+print('\nTesting incorrect job description files:')
+for jobfile in jobs_broken:
+    try:
+        job = HRM.JobDescription(jobfile, 'file')
+    except ValueError:
+        print(" - Got the excpected ValueError from '%s'." % jobfile)
